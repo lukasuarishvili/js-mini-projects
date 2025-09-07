f84e8a24d23b701d0cf8330427073f3c4a47ca78
lukasuarishvili <lukasuarishvili13@gmail.com>
2025-09-07 21:50:00 +0400

    changes for Rendering project on Render

--- register/Backend/server.js
+++ register/Backend/server.js
@@ -10,7 +10,10 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-const port = process.env.SERVER_PORT;
+
+const PORT = process.env.PORT || 3000;
+
+
 
 app.get('/', (req, res) => {
     res.send('it working');
