9e02f5567c1849c7905276ccc63f64813c6f61c1
lukasuarishvili <lukasuarishvili13@gmail.com>
2025-09-18 16:48:12 +0400

    added main feture of the citywatch(demo)

--- CityWatchDemo/src/main.js
+++ CityWatchDemo/src/main.js
@@ -1,14 +1,40 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-// Create the map and set initial view
+
+
+
 const map = L.map('map').setView([51.505, -0.09], 13);
 
-// Add OpenStreetMap tiles
+//ENV FILE KEYS
+const CITY_NAME_APY_KEY = import.meta.env.VITE_CITY_NAME_APY_KEY;
+
+// DOM ELEMENTS 
+const Submit = document.getElementById("main-button");
+
+
+async function getCityCoordinates(cityName) {
+  const data = await fetch(`https://us1.locationiq.com/v1/search.php?key=${CITY_NAME_APY_KEY}&q=${cityName}&format=json`);
+  const response = await data.json()
+  return response
+}
+
+Submit.addEventListener("click", async (event) => {
+  event.preventDefault()
+  const CityName = document.getElementById("cityname").value
+  const [CityCoordinates] = await getCityCoordinates(CityName)
+  console.log(CityCoordinates)
+  L.marker([ CityCoordinates.lat , CityCoordinates.lon])
+    .addTo(map)
+    .bindPopup('Hello')
+    .openPopup();
+})
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors',
 }).addTo(map);
 
-// Add a marker
+
+
 L.marker([51.5, -0.09])
   .addTo(map)
   .bindPopup('Hello')
